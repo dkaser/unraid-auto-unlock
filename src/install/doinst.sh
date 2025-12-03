f4f5c49774516b079ef1a3995417a20dcc60bae4
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-12-03 03:22:11 +0000

    fix: make sure event folder is created
    Signed-off-by: Derek Kaser <11674153+dkaser@users.noreply.github.com>

--- src/install/doinst.sh
+++ src/install/doinst.sh
@@ -1,3 +1,4 @@
+( mkdir -p usr/local/emhttp/plugins/auto-unlock/event )
 ( cd usr/local/emhttp/plugins/auto-unlock/event ; rm -rf array_started )
 ( cd usr/local/emhttp/plugins/auto-unlock/event ; ln -sf /usr/local/php/unraid-auto-unlock/bin/autounlock.sh array_started )
     
