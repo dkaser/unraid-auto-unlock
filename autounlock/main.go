e69800ad4471c92fca7fee4c81101587bf710a92
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-12-10 00:29:02 +0000

    refactor: split autounlock into new file
    Signed-off-by: Derek Kaser <11674153+dkaser@users.noreply.github.com>

--- autounlock/main.go
+++ autounlock/main.go
@@ -1,38 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/alexflint/go-arg"
-	"github.com/dkaser/unraid-auto-unlock/autounlock/version"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/afero"
 )
-
-type AutoUnlock struct {
-	fs   afero.Fs
-	args CmdArgs
-}
-
-func NewAutoUnlock(fs afero.Fs, args CmdArgs) (*AutoUnlock, error) {
-	// Create new instance
-	autoUnlock := AutoUnlock{
-		fs:   fs,
-		args: args,
-	}
-
-	autoUnlock.InitializeLogging()
-
-	version.OutputToDebug()
-
-	err := autoUnlock.Prechecks()
-	if err != nil {
-		return nil, fmt.Errorf("prechecks failed: %w", err)
-	}
-
-	return &autoUnlock, nil
-}
 
 func main() {
 	var args CmdArgs
