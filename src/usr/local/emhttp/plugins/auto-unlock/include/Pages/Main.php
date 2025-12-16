60333d267e85e7d8bd420627ef1ad580769b6e57
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-12-16 01:23:42 +0000

    feat: add docs link
    Signed-off-by: Derek Kaser <11674153+dkaser@users.noreply.github.com>

--- src/usr/local/emhttp/plugins/auto-unlock/include/Pages/Main.php
+++ src/usr/local/emhttp/plugins/auto-unlock/include/Pages/Main.php
@@ -96,6 +96,9 @@ $arrayStopped = Utils::isArrayStopped();
     }
 </script>
 
+<table class="unraid tablesorter"><thead><tr><td><?= $tr->tr("instructions"); ?></td></tr></thead></table>
+<p><?= $tr->tr("instructions_details"); ?> <a href="https://edac.dev/unraid/auto-unlock/setup/" target="_blank">https://edac.dev/unraid/auto-unlock/setup/</a></p>
+
 <?php
 // Show initialize section if /boot/config/plugins/auto-unlock/state.json or /boot/config/plugins/auto-unlock/unlock.enc do not exist
 if ( ! file_exists(Utils::STATE_FILE) && ! file_exists(Utils::ENC_FILE)) {
